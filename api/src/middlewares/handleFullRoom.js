4eae198a5f48482604517e4155353a50b93bceb1
Luis Victor Silva Santos <97199882+FaithfulBreeze@users.noreply.github.com>
2024-06-07 18:50:59 +0000

    adjusting files from gitod to codespace

--- api/src/middlewares/handleFullRoom.js
+++
@@ -1,8 +0,0 @@
-function handleFullRoom(req, res, next){
-    if(req.gameInstance.getIsRoomFull() == true)
-        return res.status(401).redirect('/')
-    req.gameInstance.handlePlayerJoin()
-    next()
-}
-
-module.exports = handleFullRoom
--- api/src/public/js/socket.js
+++
@@ -1,7 +0,0 @@
-const socket = io()
-
-socket.on('redirect', (roomID) =>{
-    window.location.href += roomID
-})
-
-export default socket
